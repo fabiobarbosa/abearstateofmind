5c61b2840bc6b354311b0a8b869dc53f96b9e77a
Fábio Barbosa <hello@fabiobarbosa.net>
2020-04-01 15:41:55 +0200

    Fix replace 'for' attributes with 'htmlFor' on Contact form

--- web/src/pages/contact.js
+++ web/src/pages/contact.js
@@ -26,19 +26,23 @@ const ContactPage = () => {
         <h2>Or just use this contact form.</h2>
         <p>I promise to reply within 24 hours. Your time is precious. I get that. Required fields are highlighted with an asterisk.</p>
 
-        <form name='contact' method='POST' netlify-honeypot='bot-field' data-netlify='true'>
-          <label for='form-name'>Your name</label>
+        <form name='contact' method='POST' netlify-honeypot='bot-field' data-netlify='true' action='/contact'>
+          <label htmlFor='form-name'>Your name</label>
           <input id='form-name' name='name' type='text' required placeholder={`You may use your superhero name, but should you?`} />
-          <label for='form-replyto'>Your e-mail address</label>
+          
+          <label htmlFor='form-replyto'>Your e-mail address</label>
           <input id='form-replyto' name='_replyto' type='email' required placeholder={`Yes, I'm fine even with Hotmail and AOL.`} />
 
-          <label for='form-phone'>Your phone number</label>
+          <label htmlFor='form-phone'>Your phone number</label>
           <input id='form-phone' name='telephone' placeholder={`Don't forget your country's international prefix!`} type='tel' />
 
-          <label for='form-message'>Your message</label>
-          <textarea id='form-message' required placeholder={`Type your message here…`} name='message' />
+          <label htmlFor='form-message'>Your message</label>
+          <textarea id='form-message' required placeholder={`Type your message here…`} name='message' rows='6' />
 
-          <label className='visuallyHidden'>Don’t fill this out if you're human: <input name='bot-field' /></label>
+          <label className='visuallyHidden'>
+            Don’t fill this out if you're human: 
+            <input name='bot-field' />
+          </label>
 
           <button type='submit' id='contact-submit' data-submit='…Sending'>Submit</button>
         </form>
